562d5cf0f0e41c723ff66f8c8f685fd6d87ac3f3
Anu C S <anucs2004@gmail.com>
2026-09-25 11:05:39 +0200

    Force bot test

--- src/test.js
+++ src/test.js
@@ -8,3 +8,8 @@ async function main() {
 }
 
 main();
+if (user.name = "anu") {   // assignment instead of comparison
+    console.log("Hello");
+}
+
+const result = await doSomething()   // missing try/catch
